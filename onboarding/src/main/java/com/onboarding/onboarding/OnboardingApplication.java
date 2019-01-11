5a0c5106c0b6bfc33e6be26828df7d157041bedf
pawan <je.pawangpa@gmail.com>
2019-01-11 23:12:38 +0530

    .

---
+++ onboarding/src/main/java/com/onboarding/onboarding/OnboardingApplication.java
@@ -0,0 +1,14 @@
+package com.onboarding.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingApplication.class, args);
+	}
+
+}
+
